0ea1160af01895b990b70d3c690388536f492f32
kirmiziisik <usak.sezer@gmail.com>
2022-12-02 18:31:41 +0300

    sidebar added for mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
 
 const Navbar = ({ scrollToSection, navLinks }) => {
   const { about, projects, contact } = navLinks;
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <nav>
       <a href="">
         <img src="/images/logo.png" alt="logo" />
       </a>
-      <ul className="nav-links">
+      <ul
+        className="nav-links"
+        style={isSidebarOpen ? { right: 0 } : { right: "-40rem" }}
+      >
         <li onClick={() => scrollToSection(about)}>About</li>
         <li onClick={() => scrollToSection(projects)}>Projects</li>
         <li onClick={() => scrollToSection(contact)}>Contact</li>
+        <FaTimes className="close-icon" onClick={closeSidebar} />
       </ul>
+      {isSidebarOpen || (
+        <GiHamburgerMenu className="menu-icon" onClick={openSidebar} />
+      )}
     </nav>
   );
 };
